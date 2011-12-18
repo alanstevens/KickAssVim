748686b5579447ca03b5a633f6a3b635df9467d3
Alan Stevens <alanstevens@gmail.com>
2011-12-18 11:44:04 -0500

    reordered colors and added sqlite to apt-get call

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-sudo apt-get install vim-nox exuberant-ctags ncurses-term curl git 
+sudo apt-get install vim-nox exuberant-ctags ncurses-term curl git libsqlite3-dev
 echo "source ~/.vim/vimrc" > ~/.vimrc
 echo "source ~/.vim/gvimrc" > ~/.gvimrc
 cd ~/.vim/
