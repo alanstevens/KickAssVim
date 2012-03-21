ef4c73669570418c76ba0085f96acbb17f0c9e55
Alan Stevens <alanstevens@gmail.com>
2012-03-21 01:49:25 -0400

    moved to Ruby 1.9.3 and updated scripts

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -28,15 +28,20 @@ function newgemset(){
   # Use the folder name, sans path, if no name supplied.
   local name="${PWD##*/}"
 
-  if [ $# > 0 ]; then
+  if [ "$1" != "" ]; then
     name="$1"
   fi
 
-  local ruby_version="1.9.2"
+  # echo -e "Folder name is: $name\n"
 
-  if [ $# > 1 ]; then
+  local ruby_version=`rvm-prompt v p`
+
+  if [ "$2" != "" ]; then
     ruby_version="$2"
   fi
+
+  # echo -e "Ruby version is: $ruby_version\n"
+
   # Source RVM as a function into local environment.
   if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
 
@@ -55,14 +60,47 @@ function newgemset(){
 
   fi
 
-  rvm use $ruby_version
-  rvm gemset create $name
-  rvm use $ruby_version@$name
-  echo  "rvm use $ruby_version@$name" > .rvmrc
+  # echo -e "Gemset is: $ruby_version@$name\n"
+
+  rvm --create --rvmrc use $ruby_version@$name
   rvm rvmrc trust
-  gem env gemdir
+}
+
+function newrailsapp(){
+  app_name=$1
+  echo -e "#\n# Cloning template\n#"
+
+  git clone git://github.com/NerdHiveIndustries/Rails-Template.git $app_name
+
+  cd $app_name
+
+  echo -e "#\n# Current working directory has changed to $PWD\n#"
+
+  rm -rf .git
+  rm -f Gemfile.loc
+
+  newgemset
+
+  echo -e "#\n# Installing gems via bundler\n#"
+
+  bundle install --binstubs
+
+  echo -e "#\n# Renaming rails app to $app_name\n#"
+
+  bundle exec rails g rename_to $app_name
+
+	rm -r "vendor/plugins/Rename"
+
+  rm README.markdown
+  echo "# $app_name" > README.markdown
+
+  echo -e "#\n# Creating git repository and adding files\n#"
+
+  git init
+  git add .
+  git commit -m"Initial add"
 }
 
 export -f newgemset
 export -f gvimwithtab
-
+export -f newrailsapp
--- scripts/install-rvm.sh
+++ scripts/install-rvm.sh
@@ -3,14 +3,17 @@
 bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
 
 echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
+echo '[[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion' >> ~/.bash_profile
+
+echo "install: --no-rdoc --no-ri" >> ~/.gemrc
+echo "update: --no-rdoc --no-ri" >> ~/.gemrc
 
 source ~/.bash_profile
 
-rvm install 1.9.2
+chmod +x $rvm_path/hooks/after_cd_bundler
 
-rvm use 1.9.2 --default
+rvm install 1.9.3-p125
 
-rvm use 1.9.2@global
+rvm use 1.9.3-p125 --default
 
-gem install rails --no-rdoc --no-ri
-gem install bundler --no-rdoc --no-ri
+rvm use 1.9.3-p125@global
